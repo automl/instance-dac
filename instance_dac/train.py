86ff3e174542dd8fe0e8c8600ceba2da9f514574
benjamc <c.benjamins@ai.uni-hannover.de>
2023-07-26 12:01:45 +0200

    Update

--- instance_dac/train.py
+++ instance_dac/train.py
@@ -16,44 +16,6 @@ from dacbench.wrappers import PerformanceTrackingWrapper, StateTrackingWrapper
 from dacbench.abstract_env import AbstractEnv
 
 
-
-# Run an experiment and log the results
-if __name__ == "__main__":
-
-    # Make benchmark
-    bench = SigmoidBenchmark()
-
-    # Run for 10 episodes each on 10 seeds
-    num_episodes = 10
-    seeds = range(10)
-
-    # Make logger object and add modules for performance & state logging
-    logger = Logger(
-        experiment_name="sigmoid_example",
-        output_path=Path("plotting/data"),
-        step_write_frequency=None,
-        episode_write_frequency=None,
-    )
-    state_logger = logger.add_module(StateTrackingWrapper)
-    performance_logger = logger.add_module(PerformanceTrackingWrapper)
-
-    for s in seeds:
-
-        # Make & wrap benchmark environment
-        env = bench.get_benchmark(seed=s)
-        env = PerformanceTrackingWrapper(env, logger=performance_logger)
-        env = StateTrackingWrapper(env, logger=state_logger)
-
-        # Add env to logger
-        logger.set_env(env)
-
-        # Run random agent
-        agent = RandomAgent(env)
-        run_benchmark(env, agent, num_episodes, logger)
-
-    # Close logger object
-    logger.close()
-
 def wrap_and_log(cfg: DictConfig, env: AbstractEnv) -> tuple[AbstractEnv, Logger]:
     logger = Logger(
         experiment_name="train",
