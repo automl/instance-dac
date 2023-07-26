6e55165a34d746d93ca8e1ebf11c087116d8a26a
benjamc <c.benjamins@ai.uni-hannover.de>
2023-07-26 09:57:23 +0200

    Generate

---
+++ instance_dac/__init__.py
@@ -0,0 +1,14 @@
+import datetime
+
+name = "Instance-DAC"
+package_name = "instance_dac"
+author = "benjamc"
+author_email = "c.benjamins@ai.uni-hannover.de"
+description = "No description given"
+url = "https://www.automl.org"
+project_urls = {
+    "Documentation": "https://automl.github.io/Instance-DAC/main",
+    "Source Code": "https://github.com/automl/instance_dac",
+}
+copyright = f"Copyright {datetime.date.today().strftime('%Y')}, benjamc"
+version = "0.0.1"
