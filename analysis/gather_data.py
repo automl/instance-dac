b4c56a13c64107f65cb692a9caa0ddc14a095a3c
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-01-12 15:02:26 +0100

    Update data gathjering

--- analysis/gather_data.py
+++ analysis/gather_data.py
@@ -4,5 +4,6 @@ from instance_dac.utils.data_loading import load_eval_data
 path = "../runs/Sigmoid"
 train_instance_set = "2D3M_train"
 train_instance_set_id = "sigmoid_2D3M_train"
-path = Path(path) / train_instance_set
+agent_name = "ppo"
+path = Path(path) / train_instance_set / agent_name
 data = load_eval_data(path=path, train_instance_set_id=train_instance_set_id)
