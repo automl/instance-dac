eeb6b6407dc6e273bf483fedef83874a9b27bcd7
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-01-23 21:13:53 +0100

    Wrapper for CMA-ES

---
+++ instance_dac/wrappers/cmaes_stepsize_wrapper.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from collections import OrderedDict
+
+from gymnasium.core import ActionWrapper, Env
+from gymnasium.spaces import Box
+from typing import Any
+
+
+class CMAESStepSizeWrapper(ActionWrapper):
+    def __init__(self, env: Env):
+        super().__init__(env)
+
+        self.action_space = Box(0., 10.)
+
+    def action(self, action: Any) -> Any:
+        complete_action = OrderedDict({"step_size": action})
+        return complete_action
