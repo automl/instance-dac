d62e49f32c8c573290d8d296c89e926bfcfb1771
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-01-27 09:54:05 +0100

    Fix api for both benchmarks

--- instance_dac/utils/parse_selector_sets.py
+++ instance_dac/utils/parse_selector_sets.py
@@ -23,9 +23,20 @@ def generate_instance_set(fname: str | Path):
     seed = fname.parts[9]
     selector_run = fname.name.split("_")[-1].split(".")[0]
 
+    if benchmark_id == "Sigmoid":
+        test_set_path = "../instance_sets/sigmoid/sigmoid_2D3M_test.csv"
+    else:
+        test_set_path = "../../../instance_dac/instance_sets/cmaes/seplow/test.csv"
+
     source_instance_set = Path(
         f"DACBench/dacbench/instance_sets/{benchmark_id.lower()}/{benchmark_id.lower()}_{instance_set_id}.csv"
     )
+    if not source_instance_set.is_file():
+        source_instance_set = Path(
+            f"instance_dac/instance_sets/{benchmark_id.lower().replace('-', '')}/seplow/train.csv"
+        )
+        if not source_instance_set.is_file():
+            raise ValueError("Hardcoding did not work hehe", source_instance_set)
 
     iset = pd.read_csv(fname, header=None)
     iset = iset.dropna()
@@ -40,8 +51,12 @@ def generate_instance_set(fname: str | Path):
     with open(source_instance_set, "r") as file:
         lines = file.readlines()
     # lines = [l.strip("\n") for l in lines]
-
-    filtered_set = [lines[i] for i in instance_ids]
+    offset = 0
+    if benchmark_id == "CMA-ES":
+        offset = 1
+    filtered_set = [lines[i+1] for i in instance_ids]
+    if benchmark_id == "CMA-ES":
+        filtered_set = [lines[0]] + filtered_set
 
     target_fname = (
         Path("data/instance_sets/selected/generated")
@@ -66,7 +81,7 @@ def generate_instance_set(fname: str | Path):
 benchmark:  
     config:
         instance_set_path: ../../../{instance_set_path_train}
-        test_set_path: ../instance_sets/sigmoid/sigmoid_2D3M_test.csv
+        test_set_path: {test_set_path}
 
 instance_set_id: {selector_instance_set_id}
 instance_set_selection: selector
@@ -86,7 +101,7 @@ selector:
     seed: {seed}
     run: {selector_run}
 """
-    selector_instance_set_id = f"{instance_set_id}__{selected_on}__{selection_method}__{feature_type}__{source_features}__{threshold}__{selector_run}"
+    selector_instance_set_id = f"{instance_set_id}__{selected_on}__{selection_method}__{feature_type}__{source_features}__{threshold}__{seed}__{selector_run}"
     content = template.format(
         instance_set_path_train=target_fname,
         selector_instance_set_id=selector_instance_set_id,
@@ -97,9 +112,12 @@ selector:
         threshold=threshold,
         seed=seed,
         selector_run=selector_run,
+        test_set_path=test_set_path,
     )
 
     name = selector_instance_set_id + ".yaml"
+    if benchmark_id == "CMA-ES":
+        benchmark_id = "cmaes"
     inst_config_fname = (
         Path(f"instance_dac/configs/inst/{benchmark_id.lower()}/selector") / f"source_{instance_set_id}" / name
     )
@@ -109,7 +127,7 @@ selector:
 
 if __name__ == "__main__":
     # fname = "data/selected_by_selector/Sigmoid/2D3M_train/Train/DS/Catch22/RA/0.7/dominant_0.7_1_use_params_False_sigmoid_2D3M_train_catch22.csv"
-    path = Path("data/selected_by_selector/Sigmoid/2D3M_train/Train")
+    path = Path("data/selected_by_selector/CMA-ES")
     fnames = list(path.glob("**/*.csv"))
     for fname in fnames:
         generate_instance_set(fname=fname)
